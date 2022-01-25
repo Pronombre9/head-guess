c6fd6a0fb5c3d295477759152c2bb24aea2ee697
Pronombre9 <96122451+Pronombre9@users.noreply.github.com>
2022-01-25 13:50:42 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+input.onGesture(Gesture.LogoUp, function () {
+    radio.sendString("" + (lista_de_texto._pickRandom()))
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+    game.startCountdown(10000)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("" + (lista_de_texto._pickRandom()))
+})
+let lista_de_texto: string[] = []
+radio.setGroup(784)
+lista_de_texto = [
+"koala",
+"pan",
+"pico",
+"coco",
+"minero",
+"palo",
+"huevo",
+"juguete",
+"marte",
+"satanas"
+]
 basic.forever(function () {
 	
 })
